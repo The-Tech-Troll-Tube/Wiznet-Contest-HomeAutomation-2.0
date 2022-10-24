83a9ecb55227231ec390b68ac0766244c017766e
The Tech Troll Tube <thetechtrolltube@gmail.com>
2022-10-24 16:08:12 +0530

    Leak fix

--- secrets.py
+++ secrets.py
@@ -1,17 +1,14 @@
-secrets = {
-    "ssid": "Albin-Galaxy-M31s", #Router's (AP) name
-    "password": "qwertyuiopp", #Router's (AP) password
-#    "timezone": +5,  # this is offset from UTC
-#    "github_token": "abcdefghij0123456789",
-    "aio_username" : "albinhomeautomation",   ### Wirte Username here ###
-    "aio_key" : "aio_JoqB134A79RFl0fH93OYIX0IXbmA",  ### Write Active Key here ###
-    }
+# SPDX-FileCopyrightText: 2020 Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
 
-#secrets = {
-#    "ssid": "Dr. Joseph Home WiFi Gear", #Router's (AP) name
-#    "password": "SelJose@19921995", #Router's (AP) password
-#    "timezone": -5,  # this is offset from UTC
-#    "github_token": "abcdefghij0123456789",
-#    "aio_username": "myusername", #user name for adafruit io
-#    "aio_key": "abcdefghij0123456789", #key for adafruit io
-#}
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'ssid_wifi_network',
+    'password' : 'wifi_password',
+    'aio_username' : 'my_adafruit_io_username',
+    'aio_key' : 'my_adafruit_io_key',
+    'timezone' : "Your time zone",
+    }
